b07c5c575368cfade6d27e86bfe9d5c695681cb1
rasel-chowdhury1 <rasel.chowdhury0001@gmail.com>
2024-04-12 18:39:13 +0600

    updated

--- Server/index.js
+++ Server/index.js
@@ -3,6 +3,11 @@ const app = express();
 const cors = require('cors');
 require('dotenv').config();
 const port = process.env.PORT || 3000;
+const { MongoClient, ServerApiVersion ,  ObjectId } = require('mongodb');
+// Stripe 
+// This is your test secret API key.
+const stripe = require("stripe")(process.env.PAYMENT_SECRET);
+
 
 //middleware
 app.use(cors());
@@ -10,7 +15,7 @@ app.use(express.json())
 
 //mongodb connection
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@yogo-master.dseedbw.mongodb.net/?retryWrites=true&w=majority&appName=yogo-master`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -65,12 +70,221 @@ async function run() {
       res.send(result);
     })
 
-    // update classes
-    app.put('/change-status/:id', async(req, res) => {
+    // update classes status and reason
+    app.patch('/change-status/:id', async(req, res) => {
       const id = req.params.id;
       const status = req.body.status;
       const reason = req.body.reason;
+      const filter = {_id: new ObjectId(id)}
+      const options = { upsert: true};
+      const updateDoc = {
+        $set: {
+          status: status,
+          reason: reason,
+        }
+      }
+
+      const result = await classesCollection.updateOne(filter, updateDoc, options);
+      console.log('update result -> ',result)
+      res.send(result)
     })
+
+    //get approved classes
+    app.get('/approved-classes', async(req, res) => {
+      const query = {status: 'approved'};
+      const result = await classesCollection.find(query).toArray()
+      console.log('approved classes -> ', result);
+      res.send(result);
+    } )
+
+    // get single class details
+    app.get('/single/:id', async(req, res) => {
+       const id = req.params.id;
+       const query = {_id: new ObjectId(id)};
+       const result = await classesCollection.find(query).toArray();
+       console.log('single details -> ', result);
+       res.send(result)
+    })
+
+    // update class details(all data)
+    app.put('/update-class/:id', async (req, res) => {
+      const id = req.params.id;
+      const updateClass = req.body;
+      const filter = {_id: new ObjectId(id)};
+      const options = { upsert: true};
+      const updateDoc = {
+        $set: {
+          name: updateClass.name,
+          description: updateClass.description,
+          price: updateClass.price,
+          availableSeats: parseInt(updateClass.availableSeats),
+          videoLink: updateClass.videoLink,
+          status: updateClass.status,
+        }
+      }
+
+      const result = await classesCollection.updateOne(filter,updateDoc, options);
+      res.send(result)
+    })
+
+    // Cart Routes
+    app.post('/add-to-cart', async(req, res) =>{
+      const newCartItem = req.body;
+      const result = await cartCollection.insertOne(newCartItem);
+      console.log('cart result -> ',result)
+      res.send(result)
+    })
+
+    // get cart item by classId
+    app.get('/cart-item/:id', async(req, res) => {
+      const id = req.params.id;
+      const email = req.body.email;
+      const query = {
+        classId: id,
+        email: email
+      }
+
+      const projection = {classId: 1};
+      const result = await cartCollection.findOne(query, {projection: projection});
+      res.send(result);
+    })
+
+    // cart info by user email
+    app.get('/cart/:email', async(req, res) => {
+      const email = req.params.email;
+      const query = {email : email};
+      console.log('query -> ',query)
+      const projection = {classId: 1};
+      const carts = await cartCollection.find(query, {projection: projection}).toArray();
+      console.log('carts data -> ',carts)
+      const classIds = carts.map(cart => new ObjectId(cart.classId));
+      console.log('classIds -> ',classIds)
+      const query2 = {_id: {$in: classIds}};
+      console.log('query 2 -> ',query2)
+      const result = await classesCollection.find(query2).toArray();
+      console.log('result -> ',result)
+      res.send(result);
+    })
+
+    // delete cart item
+    app.delete('/delete-cart-item/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {classId: id};
+      const result = await cartCollection.deleteOne(query);
+      res.send(result)
+    })
+
+     // PAYMENT ROUTES
+    app.post('/create-payment-intent', async (req, res) => {
+      const { price } = req.body;
+      const amount = parseInt(price) * 100;
+      const paymentIntent = await stripe.paymentIntents.create({
+          amount: amount,
+          currency: 'usd',
+          payment_method_types: ['card']
+      });
+      res.send({
+          clientSecret: paymentIntent.client_secret
+      });
+  })
+
+  // POST PAYMENT INFO 
+    app.post('/payment-info',async (req, res) => {
+      const paymentInfo = req.body;
+      const classesId = paymentInfo.classesId;
+      const userEmail = paymentInfo.userEmail;
+      const singleClassId = req.query.classId;
+      let query;
+      // const query = { classId: { $in: classesId } };
+      if (singleClassId) {
+          query = { classId: singleClassId, userMail: userEmail };
+      } else {
+          query = { classId: { $in: classesId } };
+      }
+
+      const classesQuery = { _id: { $in: classesId.map(id => new ObjectId(id)) } }
+      const classes = await classesCollection.find(classesQuery).toArray();
+      const newEnrolledData = {
+          userEmail: userEmail,
+          classesId: classesId.map(id => new ObjectId(id)),
+          transactionId: paymentInfo.transactionId,
+      }
+      const updatedDoc = {
+          $set: {
+              totalEnrolled: classes.reduce((total, current) => total + current.totalEnrolled, 0) + 1 || 0,
+              availableSeats: classes.reduce((total, current) => total + current.availableSeats, 0) - 1 || 0,
+          }
+      }
+      // const updatedInstructor = await userCollection.find()
+      const updatedResult = await classesCollection.updateMany(classesQuery, updatedDoc, { upsert: true });
+      const enrolledResult = await enrolledCollection.insertOne(newEnrolledData);
+      const deletedResult = await cartCollection.deleteMany(query);
+      const paymentResult = await paymentsCollection.insertOne(paymentInfo);
+      res.send({ paymentResult, deletedResult, enrolledResult, updatedResult });
+  })
+
+   //get payment history
+    app.get('/payment-history/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = { userEmail: email };
+      const result = await paymentsCollection.find(query).sort({ date: -1 }).toArray();
+      res.send(result);
+  })
+
+   //payment history length
+    app.get('/payment-history-length/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = { userEmail: email };
+      const total = await paymentsCollection.countDocuments(query);
+      res.send({ total });
+  })
+   
+
+  //Enrollment Routes
+  app.get("/popular_classes", async(req, res) => {
+    const result = await classesCollection.find().sort({totalEnrolled: -1}).limit(6).toArray();
+    res.send(result);
+  })
+
+  app.get("/popular-instructors", async(req, res) => {
+    const pipeline = [
+      {
+        $group: {
+          _id: "$instructorEmail",
+          totalEnrolled: { $sum: "$totalEnrolled"}
+        }
+      },
+      {
+        $lookup: {
+          from: "users",
+          localField: "_id",
+          foreignField: "email",
+          as: "instructor"
+        }
+      },
+      {
+        $project: {
+          _id: 0,
+          instructor: {
+            $arrayElemAt: ["$instructor", 0]
+          },
+          totalEnrolled: 1
+        }
+      },
+      {
+        $sort: {
+          totalEnrolled: -1
+        }
+      },
+      {
+        limit: 6
+      }
+    ];
+
+    const result = await classesCollection.aggregate(pipeline).toArray();
+    res.send(result)
+  })
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
